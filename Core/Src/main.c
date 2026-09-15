7198f7e1dfa5616bc2749f7bfa3ee0a87c3e5c60
Erick <erickmunoz13052006@gmail.com>
2026-09-15 16:40:43 -0600

    Lógica bmp

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -68,8 +68,6 @@ CAN_HandleTypeDef hcan2;
 I2C_HandleTypeDef hi2c1;
 I2C_HandleTypeDef hi2c2;
 
-SD_HandleTypeDef hsd;
-
 SPI_HandleTypeDef hspi1;
 
 UART_HandleTypeDef huart1;
@@ -85,7 +83,7 @@ float altura=0.0f;
 //------------------Calibración --------------------
 float presion_acumulacion=0.0f;
 float presion_calibrada_suelo=0.0f;
-int cont_calibracion_bmp=0;
+int cont_calibracion_bmp;
 
 //----Variables para calcular altitud --------
 
@@ -112,7 +110,6 @@ static void MX_CAN1_Init(void);
 static void MX_CAN2_Init(void);
 static void MX_I2C1_Init(void);
 static void MX_I2C2_Init(void);
-static void MX_SDIO_SD_Init(void);
 static void MX_SPI1_Init(void);
 static void MX_USART1_UART_Init(void);
 static void MX_USART6_UART_Init(void);
@@ -182,29 +179,32 @@ int main(void)
   MX_CAN2_Init();
   MX_I2C1_Init();
   MX_I2C2_Init();
-  MX_SDIO_SD_Init();
   MX_SPI1_Init();
   MX_USART1_UART_Init();
   MX_USART6_UART_Init();
   /* USER CODE BEGIN 2 */
 
 
+  HAL_GPIO_WritePin(GPIOC,GPIO_PIN_3,GPIO_PIN_SET);
 
+  HAL_Delay(1000);
+
+  HAL_GPIO_WritePin(GPIOC,GPIO_PIN_3,GPIO_PIN_RESET);
 
   //--------Inicialización del BMP280-------------
 
-  /*Si queremos utilizar I2C
+  /*Si queremos utilizar I2C*/
   bmp280.comm_mode=BMP280_MODE_I2C;
   bmp280.i2c=&hi2c1;
   bmp280.addr=BMP280_I2C_ADDRESS_0;
-   */
 
-  //Si queremos utilizar SPI
+
+ /* //Si queremos utilizar SPI
   bmp280.comm_mode=BMP280_MODE_SPI;
   bmp280.spi=&hspi1;
   bmp280.cs_port=GPIOA;
   bmp280.cs_pin=GPIO_PIN_12;
-
+*/
   bmp280_init_default_params(&parametro_bmp280);
 
   //----- Aqui va el cambio de parametros ----
@@ -231,11 +231,13 @@ int main(void)
   
 
   //----------Se calibra la altura inicial para una mejor lectura ----------
-  while(!bmp280_read_float(&bmp280,&temperatura,&presion,NULL) && cont_calibracion_bmp>100){
+  while(cont_calibracion_bmp<100){
+	  bmp280_read_float(&bmp280,&temperatura,&presion,NULL);
 	  presion_acumulacion=presion_acumulacion+presion;
+	  cont_calibracion_bmp++;
   }
 
-  presion_calibrada_suelo=presion_acumulacion/cont_calibracion_bmp;
+  presion_calibrada_suelo=presion_acumulacion/(float)cont_calibracion_bmp;
   //----- se calcula la altura con respecto a nivel del mar calibrada --------
   altura_nivel_mar=CalcularAltura(presion_calibrada_suelo,presion_nivel_mar);
 
@@ -348,8 +350,7 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
@@ -367,12 +368,12 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSE;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
   {
     Error_Handler();
   }
@@ -625,42 +626,6 @@ static void MX_I2C2_Init(void)
 }
 
 /**
-  * @brief SDIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_SDIO_SD_Init(void)
-{
-
-  /* USER CODE BEGIN SDIO_Init 0 */
-
-  /* USER CODE END SDIO_Init 0 */
-
-  /* USER CODE BEGIN SDIO_Init 1 */
-
-  /* USER CODE END SDIO_Init 1 */
-  hsd.Instance = SDIO;
-  hsd.Init.ClockEdge = SDIO_CLOCK_EDGE_RISING;
-  hsd.Init.ClockBypass = SDIO_CLOCK_BYPASS_DISABLE;
-  hsd.Init.ClockPowerSave = SDIO_CLOCK_POWER_SAVE_DISABLE;
-  hsd.Init.BusWide = SDIO_BUS_WIDE_1B;
-  hsd.Init.HardwareFlowControl = SDIO_HARDWARE_FLOW_CONTROL_DISABLE;
-  hsd.Init.ClockDiv = 0;
-  if (HAL_SD_Init(&hsd) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  if (HAL_SD_ConfigWideBusOperation(&hsd, SDIO_BUS_WIDE_4B) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN SDIO_Init 2 */
-
-  /* USER CODE END SDIO_Init 2 */
-
-}
-
-/**
   * @brief SPI1 Initialization Function
   * @param None
   * @retval None
@@ -781,7 +746,6 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
-  __HAL_RCC_GPIOD_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13|GPIO_PIN_3, GPIO_PIN_RESET);
